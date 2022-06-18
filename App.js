cea0452a4fc8fd0b9868800c50204a2ffa3623a2
Schomklin <kungchomklin@gmail.com>
2022-06-18 22:12:07 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,29 @@
+import { View, Text, StyleSheet } from 'react-native'
+import React from 'react'
+import Logo from './components/Logo';
+
+const App = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={[styles.title,styles.warning]}>Hello React Native</Text>
+      <Logo />
+    </View>
+  )
+}
+
+export default App
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignContent: 'center'
+  },
+  title: {
+    fontSize: 20,
+  },
+  warning: {
+    color: 'red'
+  }
+});
+
